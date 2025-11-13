d3a2d528d1e4d4ba3236b144ae87eccd576854b6
edmark1434 <edmarktalingting0@gmail>
2025-11-14 05:10:34 +0800

    stoped tracking of routes and actions folder

--- main/resources/js/actions/App/Http/Controllers/UserInterface/Admin/index.ts
+++
@@ -1,8 +0,0 @@
-import AdminOverviewController from './AdminOverviewController'
-import AdminMembersController from './AdminMembersController'
-const Admin = {
-    AdminOverviewController: Object.assign(AdminOverviewController, AdminOverviewController),
-AdminMembersController: Object.assign(AdminMembersController, AdminMembersController),
-}
-
-export default Admin
