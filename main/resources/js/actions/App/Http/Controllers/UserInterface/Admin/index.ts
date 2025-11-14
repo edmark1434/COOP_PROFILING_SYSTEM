2b6b7d69dbc550682255e09e304f123ae520fa9a
VinsmokeSanji69 <rondina894@gmail.com>
2025-11-14 20:14:32 +0800

    loan admin done

--- main/resources/js/actions/App/Http/Controllers/UserInterface/Admin/index.ts
+++
@@ -1,8 +0,0 @@
-import AdminOverviewController from './AdminOverviewController'
-import AdminLoanController from './AdminLoanController'
-const Admin = {
-    AdminOverviewController: Object.assign(AdminOverviewController, AdminOverviewController),
-AdminLoanController: Object.assign(AdminLoanController, AdminLoanController),
-}
-
-export default Admin
