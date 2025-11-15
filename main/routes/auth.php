8fb83d0092650f2c068d7d46263bbdf8a2fd75df
edmark1434 <edmarktalingting0@gmail>
2025-11-15 19:48:53 +0800

    fix settings

--- main/routes/auth.php
+++ main/routes/auth.php
@@ -37,11 +37,11 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('verify-email', EmailVerificationPromptController::class)
-        ->name('verification.notice');
+        ->name('verification.Appnotice');
 
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
         ->middleware(['signed', 'throttle:6,1'])
-        ->name('verification.verify');
+        ->name('verification.Appverify');
 
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
         ->middleware('throttle:6,1')
