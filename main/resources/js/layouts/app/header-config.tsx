26ad33ccbd233e07a1433df727112a2c9b360c29
jhonlynx <jpaulnoquiana2@gmail.com>
2025-11-20 23:51:09 +0800

    Member lookp up, add transaction routing, and register member button done

--- main/resources/js/layouts/app/header-config.tsx
+++ main/resources/js/layouts/app/header-config.tsx
@@ -1,9 +1,7 @@
-import {Check, Plus, Settings, Trash} from "lucide-react"
+import { Check, Plus, Settings, Trash } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import {  Link, router } from '@inertiajs/react';
+import { Link, router } from '@inertiajs/react';
 import loanApplicationForm from "@/routes/member/loanApplicationForm";
-// import { loanApplicationForm } from "@/routes/member";
-
 
 const handleMarkAllAsRead = () => {
     router.patch('/member/notifications/mark-all-as-read', {}, {
@@ -12,7 +10,6 @@ const handleMarkAllAsRead = () => {
 };
 
 export const headerConfigs: Record<string, React.ReactNode> = {
-
     "/admin/staff/": (
         <>
             <Button variant="secondary"><Settings /> Change Role</Button>
@@ -25,16 +22,21 @@ export const headerConfigs: Record<string, React.ReactNode> = {
         </>
     ),
     "/teller/transactions": (
-        <>
+        <Link href="/teller/add-transaction">
             <Button variant="secondary"><Plus /> Add Transaction</Button>
-        </>
+        </Link>
+    ),
+    "/teller/member-lookup": (
+        <Link href="/teller/register-member">
+            <Button variant="secondary"><Plus /> Register Member</Button>
+        </Link>
     ),
     "/member/profile": (
         <Button variant="destructive">Suspend</Button>
     ),
     "/member/my-loans": (
         <Link href={loanApplicationForm.get()}>
-            <Button variant="secondary" ><Plus /> Apply for loan</Button>
+            <Button variant="secondary"><Plus /> Apply for loan</Button>
         </Link>
     ),
     "/member/notifications": (
@@ -42,5 +44,4 @@ export const headerConfigs: Record<string, React.ReactNode> = {
             <Check /> Mark all as read
         </Button>
     ),
-
 }
